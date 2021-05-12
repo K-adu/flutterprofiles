f424eccd9ea201a472399a176254da98a92858a2
K-adu <manishbaral112@gmail.com>
2021-05-12 12:01:43 +0545

    database things

--- flutterprofile/lib/pages/timeline.dart
+++ flutterprofile/lib/pages/timeline.dart
@@ -12,27 +12,90 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  List<dynamic> users = [];
   @override
   void initState() {
-    getUsers();
+    // deleteUser();
+    updateUser();
+    // createUser();
+    // getUsers();
+    // getUserById();
     super.initState();
   }
 
-  getUsers() {
-    usersRef.get().then((QuerySnapshot snapshot) {
-      snapshot.docs.forEach((doc) {
-        print(doc.data());
-        print(doc.id);
-        print(doc.exists);
-      });
-    });
+  createUser() {
+    usersRef
+        .doc("asadadasd")
+        .set({"username": "jeff", "postCount": 0, "isAdmin": false});
   }
+
+  updateUser() async {
+    final doc = await usersRef.doc("FVbPr7CGXShzZUMuy6bQ").get();
+    if (doc.exists) {
+      doc.reference
+          .update({"username": "Baral", "postCount": 0, "isAdmin": false});
+    }
+  }
+
+  deleteUser() async {
+    final doc = await usersRef.doc("asadadasd").get();
+    if (doc.exists) {
+      doc.reference.delete();
+    }
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef
+        // .where("isAdmin", isEqualTo: true)
+        // .where("postsCount", isLessThan: 3)
+
+        .get();
+    setState(() {
+      users = snapshot.docs;
+    });
+
+    // usersRef.get().then((QuerySnapshot snapshot) {
+    // snapshot.docs.forEach((doc) {
+    //   print(doc.data());
+    //   print(doc.id);
+    //   print(doc.exists);
+    //   //   });
+    //   // });
+    // });
+  }
+  // getUserById() async {
+  //   final String id = "YQwxG9yvQTZvFH9FfMXD";
+  //   final DocumentSnapshot doc = await usersRef.doc(id).get();
+  //   //.then((DocumentSnapshot doc) {
+  //   print(doc.data());
+  //   print(doc.id);
+  //   print(doc.exists);
+  // }
 
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: header(context, isAppTitle: true),
-      body: linearProgress(),
-    );
+        appBar: header(context, isAppTitle: true),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: usersRef.snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return circularProgress();
+              }
+              final List<Text> children = snapshot.data.docs
+                  .map((doc) => Text(doc['username']))
+                  .toList();
+              return Container(
+                child: ListView(
+                  children: children,
+                ),
+              );
+            })
+        // body: Container(
+        //     child: ListView(
+        //   children: users.map((user) => Text(user['username'])).toList(),
+        // ),
+        // ),
+        );
   }
 }
