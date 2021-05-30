d8d2949cb50972ad34a1ac9c1534fa5a90b3643c
K-adu <manishbaral112@gmail.com>
2021-05-30 14:35:13 +0545

    profile done

--- flutterprofile/lib/pages/home.dart
+++ flutterprofile/lib/pages/home.dart
@@ -128,7 +128,7 @@ class _HomeState extends State<Home> {
             currentUser: currentUser,
           ),
           Search(),
-          Profile(),
+          Profile(profileId: currentUser?.id),
         ],
         controller: pageController,
         onPageChanged: onPageChanged,
